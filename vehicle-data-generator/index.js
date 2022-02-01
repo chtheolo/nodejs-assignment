c48e8a08172cbf535a2beaaf697064632a49565b
chtheolo <chtheolo@gmail.com>
2022-02-01 16:07:51 +0200

    Update the stream publishing messages. Use pipeline instead of pipe.

--- vehicle-data-generator/index.js
+++ vehicle-data-generator/index.js
@@ -9,22 +9,20 @@ If you're up for it
 const csvParse      = require ( "csv-parse")
 const fs            = require ( "fs")
 const Writable      = require ("stream").Writable
+const { pipeline }  = require ("stream")
+const config		= require ("./config")
 
 // NATS Server is a simple, high performance open source messaging system
 // for cloud native applications, IoT messaging, and microservices architectures.
 // https://nats.io/
 // It acts as our pub-sub (https://en.wikipedia.org/wiki/Publish%E2%80%93subscribe_pattern)
 // mechanism for other service that needs raw data
-const NATS = require("nats")
+const { connect, JSONCodec } = require("nats")
 
 // At this point, do not forget to run NATS server!
 
-// NATS connection happens here
-// After a connection is made you can start broadcasting messages (take a look at nats.publish())
-const nats = NATS.connect({json: true})
-
 // This function will start reading out csv data from file and publish it on nats
-const readOutLoud = (vehicleName) => {
+const readOutLoud = (vehicleName, nats) => {
 	// Read out meta/route.csv and turn it into readable stream
 	const fileStream = fs.createReadStream("./meta/route.csv")
 	// =========================
@@ -41,13 +39,13 @@ const readOutLoud = (vehicleName) => {
 	//  File -> parse each line to object -> published to nats
 	//
 
+	const jc = JSONCodec();
 	let i = 0
 
-	return (fileStream
-		// Filestream piped to csvParse which accept nodejs readablestreams and parses each line to a JSON object
-		.pipe(csvParse({ delimiter: ",", columns: true, cast: true }))
-		// Then it is piped to a writable streams that will push it into nats
-		.pipe(new Writable({
+	return pipeline(
+		fileStream,
+		csvParse({ delimiter: ",", columns: true, cast: true }),
+		new Writable({
 			objectMode: true,
 			write(obj, enc, cb) {
 				// setTimeout in this case is there to emulate real life situation
@@ -62,11 +60,20 @@ const readOutLoud = (vehicleName) => {
 					// The first parameter on this function is topics in which data will be broadcasted
 					// it also includes the vehicle name to seggregate data between different vehicle
 
-					nats.publish(`vehicle.${vehicleName}`, obj, cb)
-
+					nats.publish(`vehicle.${vehicleName}`, jc.encode(obj))
+					process.nextTick(cb)
 				}, Math.ceil(Math.random() * 150))
 			}
-		})))
+		}),
+		(error) => {
+			if (error) {
+				console.error(error.message)
+			}
+			else {
+				console.log("Pipeline succeeded");
+			}
+		}
+	)
 	// =========================
 	// Question Point 2:
 	// What would happend if it failed to publish to nats or connection to nats is slow?
@@ -74,10 +81,43 @@ const readOutLoud = (vehicleName) => {
 	// =========================
 }
 
-// This next few lines simulate Henk's (our favorite driver) shift
-console.log("Henk checks in on test-bus-1 starting his shift...")
-readOutLoud("test-bus-1")
-	.once("finish", () => {
-		console.log("henk is on the last stop and he is taking a cigarrete while waiting for his next trip")
-	})
-// To make your presentation interesting maybe you can make henk drive again in reverse
+
+const main = async () => {
+    try {
+        var nats = await connect(
+          // {json:true},
+          {servers: 'nats://nats:4222'}
+        );
+    } catch (error) {
+        console.error(
+            `error connecting to nats: ${error.message}`
+        );
+        return;
+    }
+
+    console.info(
+        `connected to ${nats.options.servers}`
+    );
+
+    nats.closed()
+    .then(error => {
+      console.log('connection has been closed')
+      if (error) {
+        console.error(error)
+      }
+    })
+    .catch((error) => {
+      console.dir(error)
+    })
+
+	// This next few lines simulate Henk's (our favorite driver) shift
+	console.log("Henk checks in on test-bus-1 starting his shift...")
+	readOutLoud(config.subject.name, nats)
+		.once("finish", () => {
+			console.log("henk is on the last stop and he is taking a cigarrete while waiting for his next trip")
+		})
+	// To make your presentation interesting maybe you can make henk drive again in reverse
+
+}
+
+main()
