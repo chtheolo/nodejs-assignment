13b9666ce0466bb92910f3df42c9930e3504f32a
chtheolo <chtheolo@gmail.com>
2022-02-08 13:16:35 +0200

    Move to /Nats the functionality of ws.js.

---
+++ ws_api/src/Nats/subscription.js
@@ -0,0 +1,13 @@
+const {logger} = require('../logs');
+
+async function subscribe(nats, subject) {
+	try {
+		const sub = nats.subscribe(`vehicle.${subject}`);
+		logger.info(`subscribed to ${subject} using subscription id ${sub.getID()}`);
+		return sub;
+	} catch (error) {
+		return error;
+	}
+}
+
+module.exports = {subscribe};
