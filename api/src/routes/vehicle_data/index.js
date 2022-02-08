f9a08c322fd6789ebb4aa42a68a8875312218fcd
chtheolo <chtheolo@gmail.com>
2022-02-08 15:54:30 +0200

    Call helpers functions in order to forward data to mongo.

--- api/src/routes/vehicle_data/index.js
+++ api/src/routes/vehicle_data/index.js
@@ -1,123 +1,39 @@
-const VehicleData = require('./model');
-const config = require('../../config');
+const helper = require('./helpers');
 
-function get(req, res) {
-	let query;
-
-	// Fetch all data(multiple Documents) for that vehicle.
-	if (req.query.vehicleId) {
-		console.log(req.query.vehicleId);
-		query = VehicleData.find({vehicleId: {$eq: req.query.vehicleId}});
-	}
-
-	// Fetch vehicle's data (single Document) for a specific vehicle route.
-	if (req.query.startRouteDate && req.query.vehicleId) {
-		query = VehicleData.find({
-			startRouteDate: {$eq: new Date(parseInt(req.query.startRouteDate, 10))},
-			vehicleId: {$eq: req.query.vehicleId},
-		});
-	}
-
-	// Fetch vehicle's data(multiple Documents) for a specified date.
-	if (req.query.date && req.query.vehicleId) {
-		query = VehicleData.find({
-			createdAt: {$eq: new Date(req.query.date)},
-			vehicleId: {$eq: req.query.vehicleId},
-		});
-	}
-
-	// Fetch vehicle's data(multiple Data) in from a date until now.
-	if (req.query.startDate && req.query.vehicleId) {
-		query = VehicleData.find({
-			createdAt: {$gte: new Date(req.query.startDate)},
-		});
-	}
-
-	// Fetch vehicle's data(multiple Data) in a given date range.
-	if (req.query.startDate && req.query.endDate && req.query.vehicleId) {
-		query = VehicleData.find({
-			createdAt: {$gte: new Date(req.query.startDate), $lte: new Date(req.query.endDate)},
-		});
-	}
-
-	query
-		.exec((error, data) => {
-			if (error) {
-				return res.status(422).send(error);
-			}
-
-			return res.status(200).send(data);
-		});
-}
-
-// A function that retrieves obj from the nats server and
-// save them in mongo.
-async function post(data, vehicleName) {
-	return new Promise((resolve, reject) => {
-		if (!data) {
-			reject(new Error('undefined data arguments'));
-		}
-
-		if (typeof data !== 'object') {
-			reject(new Error('You have to pass an object parameter'));
-		}
-
-		const vehicleData = new VehicleData({
-			vehicleId: vehicleName,
-			startRouteDate: new Date(data.time),
-			measurements: data,
-			energyStart: data.energy,
-			sumSpeed: data.speed,
-			odoStart: data.odo,
-			socStart: data.soc,
-		});
-
-		vehicleData.save((error, vd) => {
-			if (error) {
-				reject(error);
-			}
-
-			resolve(`Succesfully create a new Document with value: ${vd._id}`);
-		});
-	});
-}
-
-// Update data in a single Document for a specific vehicle and route.
-async function update(data) {
-	const filter = {
-		vehicleId: {$eq: config.subject.name},
-		startRouteDate: {$eq: new Date(config.routes.r1.startTime)},
-	};
-
-	const speed = () => {
-		if (data.speed === null || data.speed === undefined) {
-			return 0;
-		}
-
-		return data.speed;
-	};
-
-	const updateDoc = {
-		$push: {
-			measurements: data,
-		},
-		$inc: {
-			transactionCount: 1,
-			sumSpeed: speed(),
-		},
-		$set: {
-			energyEnd: data.energy,
-			odoEnd: data.odo,
-			socEnd: data.soc,
-		},
-	};
-
+// A HTTP GET method for vehicle_data endpoint.
+async function get(req, res) {
 	try {
-		await VehicleData.findOneAndUpdate(filter, updateDoc);
-		return 'Sucessfully updated!';
+		const data = await helper.fetch(req.query);
+		// OK
+		return res.status(200).send(data);
 	} catch (error) {
-		return error;
+		// Not found
+		return res.status(404).send(error);
 	}
 }
 
-module.exports = {get, post, update};
+// A HTTP POST method to create new Documents in mongoDB.
+async function post(req, res) {
+	try {
+		const data = await helper.create(req.body, req.params.vehicleId);
+		// Created
+		return res.status(201).send(data);
+	} catch (error) {
+		// Bad Request
+		return res.status(400).send(error);
+	}
+}
+
+// HTTP PUT methods for updating the data in a single document.
+async function put(req, res) {
+	try {
+		const data = await helper.update(req.body, req.params.vehicleId, req.params.startRouteDate);
+		// OK
+		return res.status(200).send(data);
+	} catch (error) {
+		// Bad Request
+		return res.status(400).send(error);
+	}
+}
+
+module.exports = {get, post, put};
