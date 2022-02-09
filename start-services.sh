d000c10da0f110a5e4d21a9e1e1b89cb18902910
chtheolo <chtheolo@gmail.com>
2022-02-09 20:08:20 +0200

    Add testing before start docker prod/dev.

--- start-services.sh
+++ start-services.sh
@@ -1,13 +1,17 @@
 #/bin/bash
 
-echo "Type: 1 for [ PRODUCTION  ] - (only file logs)"
-echo "Type: 2 for [ DEVELOPMENT ] - (console logs + file logs)"
+echo "Type: 1 for [ PRODUCTION  ] - (log files - production database(db) - tests)"
+echo "Type: 2 for [ DEVELOPMENT ] - (log files - develop database(db_dev) - tests)"
 read -n 1 -p "-> " "mode"
 
 if [[ $mode -eq 1 ]];
 then
+    sudo MODE=test docker-compose -f testing_websockets.yml up --build --abort-on-container-exit 
+    sudo MODE=test docker-compose -f testing_api.yml up --build --abort-on-container-exit 
     sudo MODE=start docker-compose up --build
 elif [[ $mode -eq 2 ]];
 then
+    sudo MODE=test docker-compose -f testing_websockets.yml up --build --abort-on-container-exit 
+    sudo MODE=test docker-compose -f testing_api.yml up --build --abort-on-container-exit 
     sudo MODE=dev docker-compose up --build
 fi
